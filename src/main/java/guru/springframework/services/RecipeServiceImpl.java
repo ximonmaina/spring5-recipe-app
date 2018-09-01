5973bd11fbe71b0cd837f61d2d1c95bd062e923a
XRader <simonmaina721@gmail.com>
2018-09-01 20:50:27 +0300

    Added Test classes and bootstrap and jquery support in pom.xml file

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -25,5 +26,14 @@ public class RecipeServiceImpl implements RecipeService {
          recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
          return recipeSet;
 
+    }
+
+    @Override
+    public Recipe findById(Long l) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+        if(!recipeOptional.isPresent()){
+            throw new  RuntimeException("Recipe not found");
+        }
+        return  recipeOptional.get();
     }
 }
